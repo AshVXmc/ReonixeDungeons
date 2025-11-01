971b39c389d69cbcd871c82c23aaddb4f9737862
AshVXmc <ashtonsamuel2121@gmail.com>
2025-11-02 01:22:54 +0800

    opals gained particle

--- scripts/items/LootBag.gd
+++ scripts/items/LootBag.gd
@@ -8,11 +8,17 @@ export (Dictionary) var drops_table = {
 	"sweet_herbs": 0
 }
 onready var PLAYER = get_parent().get_node("Player")
-
+onready var OPALS_GAINED_INDICATOR_PARTICLE : PackedScene = preload("res://scenes/particles/OpalsGainedIndicatorParticle.tscn")
 func _ready():
 	set_collision_mask_bit(1, true)
 	set_collision_mask_bit(5, true)
 
 func _on_Area2D_area_entered(area):
 	if area.is_in_group("Player"):
+		if opals_amount > 0:
+			var particle : OpalsGainedIndicatorParticle = OPALS_GAINED_INDICATOR_PARTICLE.instance()
+			get_parent().add_child(particle)
+			particle.position = global_position
+			particle.opals_gained = opals_amount
+			particle.play_opals_gained_animation()
 		call_deferred('free')
---
+++ scripts/particles/OpalsGainedIndicatorParticle.gd
@@ -0,0 +1,20 @@
+class_name OpalsGainedIndicatorParticle extends Node2D
+
+# 1 digit = 40 x_pos
+# 2 digits = 54 x_pos
+# 3 digits = 68 x_pos
+onready var opals_gained : int = 0
+var SPEED : int = 200
+var friction : int = 35
+var direction_shift : Vector2 = Vector2()
+
+func play_opals_gained_animation():
+	$RichTextLabel/Opal.position.x = 40 + (14 * len(str(opals_gained - 2)))
+	$RichTextLabel.bbcode_text = "+" + str(opals_gained)
+	print("opal: " + $RichTextLabel.bbcode_text)
+	$AnimationPlayer.play("opals_gained_indicator")
+	direction_shift = Vector2(rand_range(-1,1), rand_range(-1,1))
+	
+func _process(delta):
+	global_position += SPEED * direction_shift * delta
+	SPEED = max(SPEED - friction * delta , 0)
